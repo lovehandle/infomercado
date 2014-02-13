72ca85c9d8420328e5adcc868a96b5de60754bb7
Emanuel Zámano <emanuelz@emanuelz.com.mx>
2014-02-13 22:49:44 +0000

    Agregado panel para completar registro de comerciantes

--- app/views/comerciantes/dashboard.php
+++ app/views/comerciantes/dashboard.php
@@ -16,6 +16,40 @@
 	<div class="row offset-superior">
     	<h3>Bienvenido <?php print(Auth::user()->nombre);?></h3>
     </div>
+    <div class="row">
+	  <div class="panel panel-primary">
+	  <div class="panel-heading">
+	    <h3 class="panel-title">Completa tu registro</h3>
+	  </div>
+	  <div class="panel-body">
+	  	<p>Completa estos datos para terminar tu registro</p>
+	    <form class="form-horizontal" role="form">
+			<div class="form-group">
+				<label for="usuario-login" class="col-sm-2 control-label">Mercado</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="mercado-nombre" placeholder="Lagunilla Varios">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="pass-login" class="col-sm-2 control-label">Numero de Local</label>
+				<div class="col-sm-10">
+					<input type="number" class="form-control" id="local">
+				</div>
+			</div>
+			<label for="pass-login" class="col-sm-2 control-label">Categoria</label>
+				<div class="col-sm-10">
+					<input type="number" class="form-control" id="local">
+				</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+				  <a id="go-login" href="#" class="btn btn-success">Guardar</a>
+				  <div id="info-login"></div>
+				</div>
+			</div>
+		</form>
+	  </div>
+	</div><!-- panel login -->
+    </div>
     <div class="row"><a href="/comerciantes/logout">Salir</a></div>
 </div>
 </body>
