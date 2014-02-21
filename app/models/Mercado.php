2cb87e51107316fa12877f612b4d6bfccf161746
emanuelzh <emanuel.zamano@gmail.com>
2014-02-21 01:56:16 -0600

    Fix Mercado class

--- app/models/Mercado.php
+++ app/models/Mercado.php
@@ -3,7 +3,7 @@
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class Mercado extends Eloquent implements UserInterface, RemindableInterface {
+class Mercado extends Eloquent {
 	
 	protected $table = 'mercados';
 
