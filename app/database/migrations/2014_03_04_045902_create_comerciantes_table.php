ff949e36e367e1cd84502f00b0354b6063a04f17
Emanuel Zámano <emanuelz@emanuelz.com.mx>
2014-03-03 23:01:47 -0600

    migracion tabla comerciantes

---
+++ app/database/migrations/2014_03_04_045902_create_comerciantes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateComerciantesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('comerciantes', function(Blueprint $table) {
+            		$table->increments('id');
+			$table->string('nombre');
+			$table->string('password');
+			$table->integer('mercado_number');
+			$table->integer('local');
+			$table->integer('tegoria_principal');
+			$table->integer('categoria_adicional');
+			$table->string('username');
+			$table->text('servicios');
+			$table->timestamps();
+        });
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+	    Schema::drop('comerciantes');
+	}
+
+}
