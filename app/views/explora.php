ca918573160e26123c0446c13069bba50eece23f
Emanuel Zámano <emanuelz@emanuelz.com.mx>
2014-03-15 20:14:23 -0500

    pruebas de layout en home y explora chavos

--- app/views/explora.php
+++ app/views/explora.php
@@ -6,22 +6,34 @@
 <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="/css/bootstrap.css">
 <link rel="stylesheet" href="/css/main.css">
-<title>infomercado.mx - Informacion de Merados Publicos en la Ciudad de Mexico</title>
+<title>infomercado.mx</title>
 <!-- Latest compiled and minified JavaScript -->
 <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 <style>
-body{padding-top:70px;}
+.el-header{height: 80px; background-color: #f4f4f4;}
+.header-titulo{ font-size: 20pt; margin-top: 24px; color:#ea890d; display: block;}
+.el-hader a:hover, .el-header a:link{
+	color:#ea890d;
+}
+.mini-nav a{color: #FFF; font-size: 12pt; display: block; height: 60px; line-height: 64px;}
+.nav-inicio{background-color: #d8682a; }
+.nav-cerca{background-color: #ea890d; }
+.list-group{font-size: 14pt !important; margin-top: 10px;}
 </style>
 </head>
 
 <body>
 <div class="container">
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-  <div class="container" align="center">
-  	<div class="navbar-header"><p class="navbar-brand">infomercado.mx</p></div>
-  </div>
-</nav>
+<div class="row el-header" align="center">
+	<div class="col-xs-12">
+		<p class="header-titulo">infomercado.mx</p>
+	</div>
+</div>
+<div class="row mini-nav" align="center">
+	<div class="col-xs-6 nav-inicio"><a href="/">INICIO</a></div>
+	<div class="col-xs-6 nav-cerca"><a href="#">UN MERCADO CERCA</a></div>
+</div>
     <div class="list-group">
     	<?php foreach($delegaciones as $delegacion){ ?>
 	    	<a href="/mercados/<?php print($delegacion->link); ?>" class="list-group-item"><?php print($delegacion->delegacion_nombre); ?></a>
--- app/views/home-a.php
+++ app/views/home-a.php
@@ -4,43 +4,37 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
-<title>infomercado.mx - Informacion de Merados Publicos en la Ciudad de Mexico</title>
+<link rel="stylesheet" href="/css/bootstrap.css">
+<link rel="stylesheet" href="/css/main.css">
+<title>infomercado.mx</title>
 <!-- Latest compiled and minified JavaScript -->
 <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
 <style>
-*{
-	border-radius:0 !important;
-	moz-border-radius:0;
+.el-header{height: 80px; background-color: #f4f4f4;}
+.header-titulo{ font-size: 20pt; margin-top: 24px; color:#ea890d; display: block;}
+.el-hader a:hover, .el-header a:link{
+	color:#ea890d;
 }
-a, a:link, a:visited {
-	color: #000;
+a,a:hover,a:link{
+	color: #353131;
 }
-a:hover {text-decoration: underline;}
-.fondo {
-	padding: 30px;
-	font-size: 14pt;
-}
+
 .conoce {background-color: #FFDC7A;}
 .cercano {background-color: #A3CB60; padding: 30px; font-size: 16pt;}
 .explora {background-color: #F2C64A;}
 .eventos-ofertas{background-color: #80A7BF; font-size: 14pt; padding: 30px;}
-
-.headline {
-	padding: 0 10px 0 10px;
-	font-size: 8pt;
-}
-.spacer{margin-top: 2px;}
 </style>
 </head>
 
 <body>
 <div class="container">
-	<div class="row" align="center">
-    	<h2 class="site-title">infomercado.mx</h2>
-        <p class="headline">Plataforma de información y participación ciudadana en los <br>Mercados Públicos de la Ciudad de México</p>
-    </div>
+
+	<div class="row el-header" align="center">
+		<div class="col-xs-12">
+			<a class="header-titulo" href="/">infomercado.mx</a>
+		</div>
+	</div>
     <div class="row spacer" align="center">
 	    <div class="col-xs-12 cercano"><a href="cercano">MI MERCADO MÁS CERCANO</a></div>
     </div>
