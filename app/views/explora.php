bfc39628504b12f84f9a096329fc6d3d6aa81ec9
Emanuel Zámano <emanuelz@emanuelz.com.mx>
2014-03-15 19:19:32 -0500

    probando themes de bootstrap

--- app/views/explora.php
+++ app/views/explora.php
@@ -4,12 +4,12 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/bootstrap.css">
 <link rel="stylesheet" href="/css/main.css">
 <title>infomercado.mx - Informacion de Merados Publicos en la Ciudad de Mexico</title>
 <!-- Latest compiled and minified JavaScript -->
 <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
 <style>
 body{padding-top:70px;}
 </style>
@@ -19,7 +19,7 @@ body{padding-top:70px;}
 <div class="container">
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container" align="center">
-  	<p class="navbar-brand">infomercado.mx</p>
+  	<div class="navbar-header"><p class="navbar-brand">infomercado.mx</p></div>
   </div>
 </nav>
     <div class="list-group">
