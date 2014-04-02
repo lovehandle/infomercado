a42ba4a330d66436196e2e9908ffbcc437491d63
Emanuel Zámano <emanuelz@emanuelz.com.mx>
2014-04-02 00:04:31 +0000

    modificado layout del home

--- app/views/movil/home.blade.php
+++ app/views/movil/home.blade.php
@@ -3,10 +3,10 @@
 @section('contenido')
 
 <div class="row nav-home" align="center">
-	<div class="col-xs-12"><a href="/mercados/cercano">UN MERCADO CERCA</a></div>
-	<div class="col-xs-12"><a href="/explora">EXPLORA LOS MERCADOS</a></div>
-	<div class="col-xs-12"><a href="/explora">OFERTAS Y EVENTOS</a></div>
-	<div class="col-xs-12"><a href="/explora">REGISTRO DE COMERCIANTES</a></div>
+	<div class="col-xs-12" style="background-color:#FF952B;"><a href="/mercados/cercano">UN MERCADO CERCA</a></div>
+	<div class="col-xs-12" style="background-color:#FFA64D;"><a href="/explora">EXPLORA LOS MERCADOS</a></div>
+	<div class="col-xs-12" style="background-color:#FFB872;"><a href="/explora">OFERTAS Y EVENTOS</a></div>
+	<div class="col-xs-12" style="background-color:#7D9EC0;"><a href="/comerciantes">REGISTRO DE COMERCIANTES</a></div>
 </div>
 
 @stop
