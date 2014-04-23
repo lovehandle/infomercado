d323dc46c6622a568f8c7ea29b45d64d3bbafa46
Emanuel Zámano <emanuelz@emanuelz.com.mx>
2014-04-23 23:43:55 +0000

    integrado navbar closes #47

--- app/views/desktop/mini-header.blade.php
+++ app/views/desktop/mini-header.blade.php
@@ -1,5 +1,16 @@
-<div class="elheader">
+<nav class="navbar navbar-inverse" role="navigation">
     <div class="container">
-        <p class="">INFOMERCADO.MX</p>
-    </div>
-</div>
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">INFOMERCADO.MX</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="/mercados/tipos">MERCADOS POR TIPO</a></li>
+                <li><a href="/mercados/delegaciones">MERCADOS POR DELEGACION</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
